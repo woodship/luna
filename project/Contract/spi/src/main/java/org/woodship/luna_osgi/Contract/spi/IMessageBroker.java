8bd7e1d345187d37a60872213c317dfa340defeb
Killko Hon <killko@qq.com>
2013-05-03 15:37:01 +0800

    UI command broker

--- project/Contract/spi/src/main/java/org/woodship/luna_osgi/Contract/spi/IMessageBroker.java
+++ project/Contract/spi/src/main/java/org/woodship/luna_osgi/Contract/spi/IMessageBroker.java
@@ -2,10 +2,12 @@
 
 package org.woodship.luna_osgi.Contract.spi;
 
+import java.util.Map;
+
 /**
  *
  * @author Killko Hon
  */
 public interface IMessageBroker {
-
+ public Map<String,Object> exec(String cmd,Map<String,Object> param);
 }
