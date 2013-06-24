0f93378cdb9a0256f7278bcc0238d61f138d83b7
laocui <67892238@qq.com>
2013-06-24 15:16:42 +0800

    增加H2数据库管理功能

--- src/main/java/org/woodship/luna/core/security/ApplicationView.java
+++ src/main/java/org/woodship/luna/core/security/ApplicationView.java
@@ -1,31 +1,41 @@
 package org.woodship.luna.core.security;
 
+import java.sql.SQLException;
+
 import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Component;
 import org.woodship.luna.util.Utils;
 
-import com.vaadin.addon.jpacontainer.EntityProvider;
 import com.vaadin.addon.jpacontainer.JPAContainer;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.TreeTable;
+import com.vaadin.ui.VerticalLayout;
 
 @Component
 @Scope("prototype")
-public class ApplicationView extends HorizontalLayout implements View {
+public class ApplicationView extends VerticalLayout implements View {
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "application";
 
+	@Autowired
+	DataSource ds ;
+	
 	@PostConstruct
 	public void PostConstruct(){
 	    this.setSizeFull();
 	    JPAContainer<Resource> resContainer =  Utils.getHierarchialJPAContainer(Resource.class);
-		TreeTable ttable = new TreeTable("应用管理");
+		TreeTable ttable = new TreeTable();
 		ttable.setContainerDataSource(resContainer);
 		ttable.setItemCaptionPropertyId("name");
 		Utils.configTableHead(ttable, Resource.class);
@@ -35,7 +45,28 @@ public class ApplicationView extends HorizontalLayout implements View {
 		for (Object itemId: ttable.getItemIds()){
 			ttable.setCollapsed(itemId, false);
 		}
+		
+		Button startupDB = new Button("管理数据库");
+		startupDB.addClickListener(new ClickListener() {
+			private static final long serialVersionUID = 1759069585352617475L;
+			@Override
+			public void buttonClick(ClickEvent event) {
+				try {
+					org.h2.tools.Server.startWebServer(DataSourceUtils.getConnection(ds));
+				} catch (CannotGetJdbcConnectionException e) {
+					e.printStackTrace();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		HorizontalLayout buttons = new HorizontalLayout();
+		buttons.setMargin(true);
+		buttons.addComponent(startupDB);
+		
+		this.addComponent(buttons);
 		this.addComponent(ttable);
+		this.setExpandRatio(ttable, 1);
 	}
 
 	@Override
